bd19adaac5a4803ed6a3a2d0ce319fd1e1d0bdf1
Marco Lazzari <149947314+MLaz95@users.noreply.github.com>
2024-04-09 11:27:50 +0200

    stampato dati a schermo con successo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,29 @@
 @extends('layouts/app')
 
 @section('content')
+    <main>
+        <div class="container">
 
-<main>
-    <div class="container">
-        <h1 class="text-center py-5">Hello</h1>
-    </div>
-</main>
-    
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+
+                @foreach ($movies as $singleMovie)
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">{{$singleMovie->title}}</h5>
+                            <h6 class="card-subtitle text-body-secondary">{{$singleMovie->original_title}}</h6>
+                            <div class="d-flex gap-3">
+                                <div>Released on: {{$singleMovie->date}}</div>
+                                <div>{{$singleMovie->nationality}}</div>
+                                <div>{{$singleMovie->vote}}</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+
+            </div>
+
+        </div>
+    </main>
 @endsection
